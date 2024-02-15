c8930c23279a1b4283952496c3a7944d40ce3439
Jacob McAllister <mcallisterj@westerntc.edu>
2024-02-15 11:05:23 -0600

    End of class 2-15-2024
    Signed-off-by: Jacob McAllister <mcallisterj@westerntc.edu>

--- FileIOJson/Program.cs
+++ FileIOJson/Program.cs
@@ -10,22 +10,59 @@ var options = new JsonSerializerOptions() {PropertyNameCaseInsensitive = true};
 Company companyA = JsonSerializer.Deserialize<Company>(json, options);
 
 
-// Prompt 3
-const string dirName = "employees_directory";
-Directory.CreateDirectory(dirName);
+/**********************************************
+ *************** PROMPT 3 BEGIN ***************
+***********************************************/
+// const string dirName = "employees_directory";
+// Directory.CreateDirectory(dirName);
 
-Directory.SetCurrentDirectory(dirName);
+// Directory.SetCurrentDirectory(dirName);
 
-foreach(Employee person in companyA.Employees) {
-    string contents = @$"
-    ID: {person.Id}
-    Full Name: {person.FullName}
-    Annual Salary: {person.AnnualSalary}
-    Position ID: {person.Position.Id}
-    Position Description: {person.Position.Description}";
+// foreach(Employee person in companyA.Employees) {
+//     string contents = @$"
+//     ID: {person.Id}
+//     Full Name: {person.FullName}
+//     Annual Salary: {person.AnnualSalary}
+//     Position ID: {person.Position.Id}
+//     Position Description: {person.Position.Description}
+    
+//     Benefits
+//     ----------
+//     ";
 
-    File.WriteAllText($"{person.Id}.txt", contents);
-}
+//     foreach(Benefit benefit in person.Benefits){
+//         contents += @$"
+//         {benefit.Description}";
+//     }
 
-// Move back to root of project
-Directory.SetCurrentDirectory("..");
+//     File.WriteAllText($"{person.Id}.txt", contents);
+// }
+
+// // Move back to root of project
+// Directory.SetCurrentDirectory("..");
+
+/**********************************************
+ *************** PROMPT 3 END ***************
+***********************************************/
+
+
+/**********************************************
+ *************** PROMPT 9 BEGIN ***************
+***********************************************/
+
+Employee personA = companyA.Employees[0];
+Console.WriteLine($"{personA.FullName} position BEFORE: {personA.Position.Description}");
+
+personA.Position.Description = "Architect";
+
+Console.WriteLine($"{personA.FullName} position AFTER: {personA.Position.Description}");
+
+var moreOptions = new JsonSerializerOptions() {WriteIndented = true};
+
+string jsonString = JsonSerializer.Serialize(companyA, moreOptions);
+
+File.WriteAllText(jsonFile, jsonString);
+
+/**********************************************
+ *************** PROMPT 9 END ***************
+***********************************************/
