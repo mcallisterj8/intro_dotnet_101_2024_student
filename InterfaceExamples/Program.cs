e77ab83c8198dd45263fc85a16f86f2d47bd7e93
Jacob McAllister <mcallisterj@westerntc.edu>
2024-02-27 10:59:47 -0600

    End of class 2/27/2024
    Signed-off-by: Jacob McAllister <mcallisterj@westerntc.edu>

--- InterfaceExamples/Program.cs
+++ InterfaceExamples/Program.cs
@@ -1,4 +1,5 @@
 ﻿using InterfaceExamples.Models;
+using InterfaceExamples.Interfaces;
 
 iPhone phoneA = new iPhone {
                     Brand = "Apple", 
@@ -17,3 +18,44 @@ Android phoneD = new Android {
             Brand = "Google", 
             Model = "Samsung Galaxy 12"
         };
+
+// phoneA.BrowseInternet(); // iPhone
+// phoneB.BrowseInternet(); // Android
+
+// phoneA.UseAssistant("Check Weather"); // Valid command
+// phoneA.UseAssistant("Test"); // Invalid command
+
+
+ISmartphone phoneE = new iPhone {
+                        Brand = "Apple",
+                        Model = "iPhone 16"
+                    };
+
+ISmartphone phoneF = new Android {
+                        Brand = "Google",
+                        Model = "Samsung Galaxy 20"
+                    };
+
+// phoneE.BrowseInternet();
+// phoneF.BrowseInternet();
+
+// phoneE = new Android {
+//             Brand = "Google",
+//             Model = "Samsung Galaxy 208"
+//         };
+// phoneE.BrowseInternet();
+
+// phoneA.CallSteveJobs();
+
+/////////////////////////////////////
+
+List<ISmartphone> smartphones = new List<ISmartphone>{
+    phoneA,
+    phoneB,
+    phoneC,
+    phoneD
+};
+
+foreach(ISmartphone phone in smartphones) {
+    phone.BrowseInternet();
+}
