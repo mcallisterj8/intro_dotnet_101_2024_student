8ebf54b9161469b43c0442a252018553ef748fce
Jacob McAllister <mcallisterj@westerntc.edu>
2024-04-04 10:20:06 -0500

    End of class 4-4-2024
    Signed-off-by: Jacob McAllister <mcallisterj@westerntc.edu>

--- GenericLogger/Program.cs
+++ GenericLogger/Program.cs
@@ -4,5 +4,8 @@ User userA = new User { Name = "Alice", Age = 27 };
 
 var logger = new Logger("Wall-E");
 logger.Log("User logged in", 42);
-logger.Log(DateTime.Now, userA);
+
+Console.WriteLine("===========================");
+
+// logger.Log(DateTime.Now, userA);
 logger.DisplayLogCount();
---
+++ Questions/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("================================");
+Console.WriteLine(Directory.GetCurrentDirectory());
+Console.WriteLine("================================");
+
+// Create a directory
+string writeFolder = "Output";
+Directory.CreateDirectory(writeFolder);
+
+// Move into that directory
+Directory.SetCurrentDirectory(writeFolder);
+
+Console.WriteLine("================================");
+Console.WriteLine(Directory.GetCurrentDirectory());
+Console.WriteLine("================================");
+
+// Now, write in that directory
+string content = "Hello World!";
+File.WriteAllText("test.txt", content);
