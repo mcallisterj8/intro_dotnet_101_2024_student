cb02c1819882d80862687e79bcc133ff9c354f97
Jacob McAllister <mcallisterj@westerntc.edu>
2024-04-09 10:24:14 -0500

    End of class 4-9-2024
    Signed-off-by: Jacob McAllister <mcallisterj@westerntc.edu>

---
+++ DicionaryExamples/Program.cs
@@ -0,0 +1,36 @@
+﻿
+var capitals = new Dictionary<string, string>();
+
+capitals.Add("France", "Paris");
+capitals.Add("Japan", "Tokyo");
+
+if( capitals.TryAdd("Germany", "Berlin") ) {
+    Console.WriteLine($"Added element!");
+} else {
+    Console.WriteLine($"Did NOT add element!");
+}
+
+if( capitals.TryAdd("France", "Paris") ) {
+    Console.WriteLine($"Added element!");
+} else {
+    Console.WriteLine($"Did NOT add element!");
+}
+
+foreach(KeyValuePair<string, string> kvp in capitals) {
+    Console.WriteLine($"{kvp.Key} => {kvp.Value}");
+
+}
+
+Console.WriteLine("================= ITERATING KEYS BEGIN =================");
+foreach (var key in capitals.Keys) {
+    Console.WriteLine($"Country: {key}");
+}
+
+Console.WriteLine("================= ITERATING KEYS END =================");
+
+Console.WriteLine("\n================= ITERATING VALUES BEGIN =================");
+foreach (var value in capitals.Values) {
+    Console.WriteLine($"Capital: {value}");
+}
+
+Console.WriteLine("================= ITERATING VALUES END =================");
